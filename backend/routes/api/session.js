a3a78cbacb00356c340bb9915841dd0fb734c5bb
gjinhung <gjinhung2@gmail.com>
2023-04-19 21:57:50 -0400

    complete signup validation

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const { Op } = require('sequelize');
 const bcrypt = require('bcryptjs');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
