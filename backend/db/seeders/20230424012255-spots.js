f12200bca0de0b0e863d517889317d0d08b5d702
gjinhung <gjinhung2@gmail.com>
2023-04-30 19:46:13 -0400

    third attempt

--- backend/db/seeders/20230424012255-spots.js
+++ backend/db/seeders/20230424012255-spots.js
@@ -5,85 +5,87 @@ if (process.env.NODE_ENV === "production"){
   options.schema = process.env.SCHEMA;
   }
 
-  const { Spot, User, SpotImage } = require('../models');
-
-  const spots = [
-    {
-      owner: "ashketchum",
-      address: "123 Ash Street",
-      city: "Pallet Town",
-      state: "Kanto",
-      country: "USA",
-      lat: 35.546242,
-      lng: 139.450638,
-      name: "Ash's House",
-      description: "This house is where the player lives before beginning their journey.",
-      price: 30,
-      avgRating: 0,
-      previewImg: ""
-  },
-  {
-    owner: "brockharrison",
-    address: "123 Brock Street",
-    city: "Pewter City",
-    state: "Kanto",
-    country: "USA",
-    lat: 36.392700,
-    lng: 139.072693, 
-    name: "Pewter Gym",
-    description: "The Pewter Gym is the official Gym of Pewter City. It is based on Rock-type Pokémon",
-    price: 20,
-    avgRating: 0,
-    previewImg: ""
-  },
-  {
-    owner: "mistywilliams",
-    address: "123 Misty Street",
-    city: "Cerulean City",
-    state: "Kanto",
-    country: "USA",
-    lat: 36.549980,
-    lng: 139.870010, 
-    name: "Cerulean Gym",
-    description: "The Cerulean Gym is the official Gym of Cerulean City. It is based on Water-type Pokémon.",
-    price: 30,
-    avgRating: 0,
-    previewImg: ""
-  }
-]
-
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    try {
-      for (let spotInfo of spots) {
-        const { address, city, state, country, lat, lng, name, description, price, previewImg, avgRating} = spotInfo;
-        const foundUser = await User.findOne({
-          where: { username: spotInfo.owner}
-        });
-        await Spot.create({
-          address,
-          city,
-          state,
-          country,
-          lat,
-          lng,
-          name,
-          description,
-          price,
-          avgRating,
-          previewImg,
-          ownerId: foundUser.id
-        });
-      
-      }
-    } catch(err) {
-      console.error(err);
-      throw err;
+    return queryInterface.bulkInsert(options, [
+      {
+        ownerId: 1,
+        address: "123 Ash Street",
+        city: "Pallet Town",
+        state: "Kanto",
+        country: "USA",
+        lat: 35.546242,
+        lng: 139.450638,
+        name: "Ash's House",
+        description: "This house is where the player lives before beginning their journey.",
+        price: 30,
+        avgRating: 0,
+        previewImg: ""
+    },
+    {
+      ownerId: 2,
+      address: "123 Brock Street",
+      city: "Pewter City",
+      state: "Kanto",
+      country: "USA",
+      lat: 36.392700,
+      lng: 139.072693, 
+      name: "Pewter Gym",
+      description: "The Pewter Gym is the official Gym of Pewter City. It is based on Rock-type Pokémon",
+      price: 20,
+      avgRating: 0,
+      previewImg: ""
+    },
+    {
+      ownerId: 3,
+      address: "123 Misty Street",
+      city: "Cerulean City",
+      state: "Kanto",
+      country: "USA",
+      lat: 36.549980,
+      lng: 139.870010, 
+      name: "Cerulean Gym",
+      description: "The Cerulean Gym is the official Gym of Cerulean City. It is based on Water-type Pokémon.",
+      price: 30,
+      avgRating: 0,
+      previewImg: ""
     }
+  ], {});
   },
+    // try {
+    //   for (let spotInfo of spots) {
+    //     const { address, city, state, country, lat, lng, name, description, price, previewImg, avgRating} = spotInfo;
+    //     const foundUser = await User.findOne({
+    //       where: { username: spotInfo.owner}
+    //     });
+    //     await Spot.create({
+    //       address,
+    //       city,
+    //       state,
+    //       country,
+    //       lat,
+    //       lng,
+    //       name,
+    //       description,
+    //       price,
+    //       avgRating,
+    //       previewImg,
+    //       ownerId: foundUser.id
+    //     });
+      
+    //   }
+    // } catch(err) {
+    //   console.error(err);
+    //   throw err;
+    // }
+  // },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('Spots', {name: spots.map(spot => spot.name)}, {});
+    options.tableName = 'Spots';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      name: { [Op.in]: ['Cerulean Gym', 'Pewter Gym', "Ash's House"] }
+    }, {});
   }
 };
--- backend/db/seeders/20230424012256-bookings.js
+++ backend/db/seeders/20230424012256-bookings.js
@@ -5,46 +5,66 @@ if (process.env.NODE_ENV === "production"){
   options.schema = process.env.SCHEMA;
   }
 
-  const { Booking, User } = require('../models');
+  // const { Booking, User } = require('../models');
 
-  const bookings = [
+  // const bookings = [
+  //   {
+  //     user: "ashketchum",
+  //     spotId: 3,
+  //     startDate: "2024-01-01",
+  //     endDate:"2024-01-07"
+  //   },
+  //   {
+  //     user: "ashketchum",
+  //     spotId: 2,
+  //     startDate: "2024-01-07",
+  //     endDate: "2024-01-14"
+  //   }
+  // ]
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return queryInterface.bulkInsert(options, [
     {
-      user: "ashketchum",
+      userId: 1,
       spotId: 3,
       startDate: "2024-01-01",
       endDate:"2024-01-07"
     },
     {
-      user: "ashketchum",
+      userId: 1,
       spotId: 2,
       startDate: "2024-01-07",
       endDate: "2024-01-14"
     }
-  ]
 
-/** @type {import('sequelize-cli').Migration} */
-module.exports = {
-  async up (queryInterface, Sequelize) {
-    try {
-      for (let bookingInfo of bookings) {
-        const { startDate, endDate, spotId} = bookingInfo;
-        const foundUser = await User.findOne({
-          where: { username: bookingInfo.user}
-        });
-        await Booking.create({
-          startDate,
-          endDate,
-          userId: foundUser.id,
-          spotId
-        });
-      }
-    } catch(err) {
-      console.error(err);
-      throw err;
-    }  
-  },
+    // try {
+    //   for (let bookingInfo of bookings) {
+    //     const { startDate, endDate, spotId} = bookingInfo;
+    //     const foundUser = await User.findOne({
+    //       where: { username: bookingInfo.user}
+    //     });
+    //     await Booking.create({
+    //       startDate,
+    //       endDate,
+    //       userId: foundUser.id,
+    //       spotId
+    //     });
+    //   }
+    // } catch(err) {
+    //   console.error(err);
+    //   throw err;
+    // }  
+  ])},
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('Bookings', { user: bookings.map(booking => booking.user) }, {});
+  
+    options.tableName = 'Bookings';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      spotId: { [Op.in]: [3, 2] }
+    }, {});
+
   }
 };
