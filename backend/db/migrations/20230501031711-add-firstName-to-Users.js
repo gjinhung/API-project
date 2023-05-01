69cafb06118d38aff8b6137d3976cc3eef5c5f68
gjinhung <gjinhung2@gmail.com>
2023-05-01 01:09:46 -0400

    lastlast

---
+++ backend/db/migrations/20230501031711-add-firstName-to-Users.js
@@ -0,0 +1,32 @@
+'use strict';
+
+let options = {};
+if (process.env.NODE_ENV === "production"){
+options.schema = process.env.SCHEMA;
+}
+
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    try {
+      await queryInterface.addColumn('User', 'firstName', {
+        type: Sequelize.STRING
+      });
+      await queryInterface.addColumn('User', 'lastName', {
+        type: Sequelize.STRING
+      });
+      return Promise.resolve();
+    } catch (e) {
+      return Promise.reject(e);
+    }
+  },
+async down(queryInterface, Sequelize) {
+  try{
+  await queryInterface.removeColumn('User', 'firstName');
+  await queryInterface.removeColumn('User', 'lastName');
+  return Promise.resolve();
+} catch (e) {
+  return Promise.reject(e);
+}
+};
