40689c231b5b912c0fc08d27515a6dae5f177da5
gjinhung <gjinhung2@gmail.com>
2023-04-30 23:59:14 -0400

    updated mig

--- backend/db/migrations/20230501031711-add-firstName-to-Users.js
+++ backend/db/migrations/20230501031711-add-firstName-to-Users.js
@@ -1,20 +1,20 @@
 'use strict';
 
 let options = {};
-// if (process.env.NODE_ENV === "production"){
-// options.schema = process.env.SCHEMA;
-// }
+if (process.env.NODE_ENV === "production"){
+options.schema = process.env.SCHEMA;
+}
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn('Users', 'firstName', {
+    await queryInterface.addColumn('User', 'firstName', {
         type: Sequelize.STRING,
         allowNull: false
   },options)
 },
 async down(queryInterface, Sequelize) {
-  options.tableName = "Users";
-  await queryInterface.removeColumn("Users", "firstName",options)
+  options.tableName = 'Users';
+  await queryInterface.removeColumn('Users', "firstName", options)
 }
 };
